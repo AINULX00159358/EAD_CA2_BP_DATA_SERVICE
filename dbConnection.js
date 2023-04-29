74bb2902a54b8f8fe77de733306dbc5a4a19f821
Ainul.Habib <habib.ainul@fmr.com>
2023-04-29 12:18:39 +0100

    updated cofig

--- dbConnection.js
+++ dbConnection.js
@@ -3,27 +3,23 @@ const fs = require("fs");
 const { MongoClient } = require('mongodb');
 
 const getDBUri =  (config) => new Promise((resolve, reject) => {
-    if (process.env.MONGO_CONN_URI) {
-        console.log('reading from env var MONGO_CONN_URI ' );
-        if (process.env.MONGO_CREDENTIALS){
-            resolve("mongodb://"+process.env.MONGO_CREDENTIALS+"@"+process.env.MONGO_CONN_URI);
-            return;
-        }
-        throw(new Error("ERROR: Missing Mongo Credentials "))
+    if (process.env.MONGO_CONN_URL) {
+        resolve(process.env.MONGO_CONN_URL);
+        return;
     }
 
-    if (config.database.credentials) {
-        console.log(' connection using database development credentials');
-        resolve("mongodb://"+ config.database.credentials+"@"+config.database.connectionUri);
+    if (config.configID === 'development') {
+        console.log('connection using database development credentials');
+        resolve(config.database.connectionUrl);
         return;
     }
 
     let credential =  fs.readFileSync('/etc/mongo-secrets/MONGO_CREDENTIAL','utf8');
     if (credential){
         credential = credential.replace(/\r|\n/g, '')
-        let mongoUrl = "mongodb://"+ credential +"@"+config.database.connectionUri;
-        console.log(' connection using file /etc/mongo-secrets/**');
-        resolve(mongoUrl);
+        let mongoUrl = config.database.connectionUrl;
+        console.log(' connection using file /etc/mongo-secrets/** ');
+        resolve(mongoUrl.replace("CREDENTIALS", credential));
         return;
     }
     throw(new Error("ERROR: Cannot Strat the Application, Unable to get credentials "))
